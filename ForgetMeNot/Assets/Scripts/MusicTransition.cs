e5f100014459b8e828fc13c7de289ae9a51ffa91
Keenan Curry <58228738+TrueEquinox@users.noreply.github.com>
2021-03-24 15:05:22 -0400

    Revert "Updating Branch"

--- ForgetMeNot/Assets/Scripts/MusicTransition.cs
+++ ForgetMeNot/Assets/Scripts/MusicTransition.cs
@@ -1,14 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicTransition : MonoBehaviour
 {
     private static MusicTransition instance;
-    public AudioSource audioSource;
-    public AudioClip clip1;
-    public AudioClip clip2;
 
     void Awake()
     {
@@ -21,34 +17,6 @@ public class MusicTransition : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    void Update()
-    {
-        if (SceneManager.GetActiveScene().name == "Bedroom")
-        {
-            if (audioSource.clip!=clip1)
-            {
-                changeClip(clip1);
-            }
-        }
-        else
-        {
-            if (audioSource.clip!=clip2) {
-                changeClip(clip2);
-            }
-        }
-    }
-
-    void changeClip(AudioClip clip) {
-        audioSource.Stop();
-        audioSource.clip = clip;
-        audioSource.Play();
     }
 }
 
