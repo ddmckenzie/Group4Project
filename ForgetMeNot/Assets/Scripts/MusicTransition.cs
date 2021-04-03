f722c40c6dfd9c437e7f85b58a61d1ff64b44b19
cadriamal <70038882+cadriamal@users.noreply.github.com>
2021-04-03 02:04:37 -0400

    Trying to fix fade glitch

--- ForgetMeNot/Assets/Scripts/MusicTransition.cs
+++ ForgetMeNot/Assets/Scripts/MusicTransition.cs
@@ -50,20 +50,6 @@ public class MusicTransition : MonoBehaviour
         audioSource.clip = clip;
         audioSource.Play();
     }
-
-    IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
-    {
-        float currentTime = 0;
-        float start = audioSource.volume;
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
-            yield return null;
-        }
-        yield break;
-    }
 }
 
 //Based on tutorial by Blackthornprod
