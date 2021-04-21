efe07ef321034f665035d811fcc27f819d5d6074
cadriamal <70038882+cadriamal@users.noreply.github.com>
2021-04-21 15:21:25 -0400

    Started code to get doors to lock if necessary

--- ForgetMeNot/Assets/Door.cs
+++ ForgetMeNot/Assets/Door.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public LevelLoader loader;
-    public Manager manager;
+    public GameObject doors;
+    int doorsUnlocked;
+
+    void Start()
+    {
+        doorsUnlocked = PlayerPrefs.GetInt("DoorsUnlocked");
+        doors = GameObject.Find("Doors");
+        doors.transform.GetChild(0).GetComponent<BoxCollider>().enabled = true;
+        Debug.Log("Bedroom Unlocked");
+
+        for (int i = 1; i < doors.transform.childCount; i++)
+        {
+            doors.transform.GetChild(i).GetComponent<BoxCollider>().enabled = false;
+        }
+
+        switch (doorsUnlocked)
+        {
+            case 1:
+                doors.transform.GetChild(1).GetComponent<BoxCollider>().enabled = true;
+                Debug.Log("Office Unlocked");
+                break;
+
+            case 2:
+                doors.transform.GetChild(1).GetComponent<BoxCollider>().enabled = true;
+                doors.transform.GetChild(3).GetComponent<BoxCollider>().enabled = true;
+                Debug.Log("Office and Lab Unlocked");
+                break;
+
+            case 3:
+                for (int i = 1; i < doors.transform.childCount; i++)
+                {
+                    doors.transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
+                }
+                Debug.Log("All rooms are unlocked");
+                break;
+        }
+        
+    }
 
     void OnMouseDown()
     {
         int index = transform.GetSiblingIndex();
-        //Debug.Log("Index is " + index);
-        //if (manager.doorsUnlocked[index] == true)
-        //{
-        //    loader.LoadRoom(index + 2); //Removes mainmenu and hallway as a choice
-        //}
-        //else
-        //{
-        //Send message that door is locked
-        //    Debug.Log("This door is locked");
-        //}
+        
         loader.LoadRoom(index + 2);
         
+    }
+
+    public void resetProgress() {
+        for (int i = 1; i < doors.transform.childCount; i++)
+        {
+            doors.transform.GetChild(i).GetComponent<BoxCollider>().enabled = false;
+        }
+        PlayerPrefs.DeleteAll();
     }
 }
